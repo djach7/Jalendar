e9443085ee9189d1e410c5b876cf4432baa31a22
djach7 <dave.jach7@gmail.com>
2024-06-03 18:39:58 -0400

    added comments

--- jate.py
+++ jate.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+# Constants of the Jalendar system
 DAYS_IN_YEAR = 1461
 MINUTES_IN_YEAR = 525960
 MINUTES_IN_DAY = 360
@@ -10,13 +11,19 @@ class Jate:
     def getJate(self, irlTime):
         irlTimePD = pd.to_datetime(irlTime)
 
+        # Get minutes since start of Jalendar to base all dates from
         minutesSinceBirth = pd.Timedelta(irlTimePD - JON_BIRTH_PD) / pd.Timedelta(minutes=1)
 
+        # Current Jon Year equals the minutes since JON_BIRTH divided by the minutes in a Jon Year
         curJYear = str(int(minutesSinceBirth / MINUTES_IN_YEAR))
+        # Current Jon Day equals the remainder of the JYear division divided by minutes in a Jon Day
         curJDay = str(int((minutesSinceBirth % MINUTES_IN_YEAR) / MINUTES_IN_DAY))
+        # Current Jon Minute equals the remainder of the JDay division
         curJMin = str(int((minutesSinceBirth % MINUTES_IN_YEAR) % MINUTES_IN_DAY))
+        # Current Jon Second equals normal seconds, since Jon Seconds are not different
         curJSec = irlTimePD.second
         
+        # Conditional adds a zero for prettier printing of time
         if curJSec < 10:
             curJSec = str("0" + str(curJSec))
         else:
--- main.py
+++ main.py
@@ -6,6 +6,12 @@ from datetime import datetime
 from jate import Jate
 import pandas as pd
 
+###
+### Welcome to the Jalendar Bot. This bot converts normal dates and times into
+### their corresponding Jates and Jimes according to the Jon Calendar (Jalender).
+### Please use responsibly, enjoy!
+###
+
 load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
@@ -14,16 +20,24 @@ bot = commands.Bot(command_prefix='?', intents=intents)
 
 jateObj = Jate()
 
+# On bot run, log connection to discord
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
 
+# On jate command, bot prints current jate and jime in discord channel of command
+# Example discord input: ?jate
 @bot.command()
 async def jate(ctx):
     current_time = datetime.now()
     curJate = jateObj.getJate(current_time)
     await ctx.send("Today's jate is " + curJate[0] + "/" + curJate[1] + " " + curJate[2] + ":" + curJate[3] + " IJ")
 
+# On futureJate command, bot prints irl date specified and the corresponding jate and jime
+# Time is optionally specified on input
+# Example discord input: ?futureJate 6/7/24 18:30
+# Example discord input: ?futureJate 6/7/2024
+# Example discord input: ?futureJate 6/7/24
 @bot.command()
 async def futureJate(ctx, *, fDate: str):
     fJate = jateObj.getJate(pd.to_datetime(fDate))
