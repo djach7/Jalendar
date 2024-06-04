0da5010a7cd4d2749adf3ed231d90ce29c8812ff
djach7 <dave.jach7@gmail.com>
2024-06-03 23:38:37 -0400

    added special messages for jon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import random
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands, tasks
@@ -30,6 +31,9 @@ async def on_ready():
 # Example discord input: ?jate
 @bot.command()
 async def jate(ctx):
+    jonny = isItJonny(ctx.author.name)
+    if jonny[0]:
+        await ctx.send(jonny[1])
     current_time = datetime.now()
     curJate = jateObj.getJate(current_time)
     # curJate is returned as [curJWeekday, curJWeek, curJDay, curJYear, curJMin, curJSec]
@@ -42,12 +46,18 @@ async def jate(ctx):
 # Example discord input: ?futureJate 6/7/24
 @bot.command()
 async def futureJate(ctx, *, fDate: str):
+    jonny = isItJonny(ctx.author.name)
+    if jonny[0]:
+        await ctx.send(jonny[1])
     fJate = jateObj.getJate(pd.to_datetime(fDate))
     # fJate is returned as [curJWeekday, curJWeek, curJDay, curJYear, curJMin, curJSec]
     await ctx.send("The date " + fDate + " is " + fJate[0] + ", " + fJate[1] + "/" + fJate[2] + "/" + fJate[3] + " " + fJate[4] + ":" + fJate[5] + " IJ on the Jalendar")
 
 @bot.command()
 async def jelp(ctx):
+    jonny = isItJonny(ctx.author.name)
+    if jonny[0]:
+        await ctx.send(jonny[1])
     await ctx.send("""Welcome to the Jalendar! You found the hidden help message! Valid commands include: 
     To print the current jate: `?jate`
     To print a specified jate: `?futureJate 11/6/24 11:15`
@@ -56,11 +66,32 @@ async def jelp(ctx):
     
 @bot.command()
 async def help(ctx):
+    jonny = isItJonny(ctx.author.name)
+    if jonny[0]:
+        await ctx.send(jonny[1])
     await ctx.send("""Welcome to the Jalendar! Valid commands include: 
     To print the current jate: `?jate`
     To print a specified jate: `?futureJate 11/6/24 11:15`
     To print this help message again: `?help`
                    """)
+
+def isItJonny(user):
+    isJonny = False
+    if user == "jflatley22":
+        isJonny = True
+
+    messageOptions = [
+        "Hello benevolent overlord! \n",
+        "Thank you for gracing us with your presence. \n",
+        "It is a pleasure to interact with you today, Sir. \n",
+        "It is an honor to serve you."
+    ]
+
+    retMessage = ""
+    if isJonny:
+        retMessage = random.choice (messageOptions)
+
+    return [isJonny, retMessage]
 
 
 bot.run(DISCORD_TOKEN)
